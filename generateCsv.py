e68c4425151a49ac2d91f311bb6c5f477c45a49e
wkim <wkim@qualcomm.com>
2019-12-10 21:13:00 -0800

    initial insert

---
+++ generateCsv.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+
+data_folders = ['block', 'fakeskyblock', 'skyblock']
+
+filenames = [os.listdir(f) for f in data_folders]
+[print(f[1]) for f in filenames]
+[len(f) for f in filenames]
+
+files_dict = dict(zip(data_folders, filenames))
+base_gcs_path = 'gs://cloudml-demo-vcm/ftc_ml/'
+
+# What we want:
+# gs://cloudml-demo-vcm/chairs_table_bike/chair_black/chair_black157.jpg, 'chair_black'
+# base_gcs_path + dict_key + '/' + filename
+
+data_array = []
+
+for (dict_key, files_list) in files_dict.items():
+    for filename in files_list:
+        #         print(base_gcs_path + dict_key + '/' + filename)
+        if '.jpg' not in filename:
+            continue  # don't include non-photos
+
+        label = dict_key
+        #         label = 'chair' if 'chair' in dict_key else dict_key # for grouping all chairs as one label
+
+        data_array.append((base_gcs_path + dict_key + '/' + filename, label))
+
+dataframe = pd.DataFrame(data_array)
+dataframe.to_csv('all_data.csv', index=False, header=False)
+
+#run following command
+#gsutil cp all_data.csv gs://cloudml-demo-vcm/chairs_table_bike/
